c721e1620555a4e63ba7501a97cf6c0dbabcaee8
Thana Hongsuwan <khthana@hotmail.com>
2026-01-14 14:24:18 +0700

    modify date.py

--- date.py
+++ date.py
@@ -13,3 +13,23 @@ def day_of_year(day, month, year):
 
     day_of_years = sum(days_in_month[:month - 1]) + day
     return day_of_years
+
+def day_in_year(year):
+    return 366 if is_leap(year) else 365
+
+def date_diff(date1, date2):
+
+    d1, m1, y1 = map(int, date1.split("-"))
+    d2, m2, y2 = map(int, date2.split("-"))
+
+    if y1 == y2:
+        return day_of_year(d2, m2, y2) - day_of_year(d1, m1, y1) + 1
+    days = day_in_year(y1) - day_of_year(d1, m1, y1) + 1
+
+    for year in range(y1 + 1, y2):
+        days += day_in_year(year)
+
+    days += day_of_year(d2, m2, y2)
+
+    return days
+
